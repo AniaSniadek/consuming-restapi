41d8fa13bd1b094631d22184dafb7bb63720de88
Speedy09 <sniadek97@gmail.com>
2020-04-15 15:13:06 +0200

    show users

---
+++ src/components/ShowUsers.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import User from './User';
+
+const readUsers = 'http://127.0.0.1/calendar/users/read.php';
+
+class ShowUsers extends Component {
+    state = {
+        usersList: []
+    }
+
+    handleFetchDataRead = () => {
+        fetch(readUsers)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    usersList: data.records
+                })
+            })
+    }
+
+    render() {
+        const users = this.state.usersList.map(user => (
+            <User key={user.id} name={user.name} email={user.email} password={user.password} />
+        ))
+        return (
+            <div>
+                <button onClick={this.handleFetchDataRead}>Show users</button>
+                <ul>{users}</ul>
+            </div>
+        );
+    }
+}
+
+export default ShowUsers;
